5e4d25cf088f3d8aa77a53e988c7569687927051
Óliver García Albertos <3skuero@gmail.com>
2018-09-29 11:58:03 +0100

    syncall.py: Add Gradle build option

--- syncall/syncall.py
+++ syncall/syncall.py
@@ -5,16 +5,20 @@ import git
 import platform
 import subprocess
 clean = False
+build = False
 command = "gradlew"
 if "Windows" in platform.system():
 	command += ".bat"
 for arg in sys.argv:
 	if arg == "--clean":
 		clean = True
+	if arg == "--build":
+		build = True
 projects = os.listdir(".")
 updated = ""
 for project in projects:
 	if os.path.isdir(project) and ".git" in os.listdir(project):
+		changed = False
 		os.chdir(project)
 		repo = git.Repo(".").git
 		lastdate = repo.log("-n", "1", "--format=%cr")
@@ -24,9 +28,14 @@ for project in projects:
 		result = repo.pull()
 		if "Already up" not in result:
 			updated += " " + project
+			changed = True
 		print(result + "\n")
-		if clean and command in os.listdir("."):
-			print("CLEANING GRADLE CACHE")
-			subprocess.call(["./" + command, " clean"])
+		if command in os.listdir("."):
+			if clean:
+				print("CLEANING GRADLE CACHE")
+				subprocess.call(["./" + command, "clean"])
+			if build and changed:
+				print("BUILDING GRADLE APP")
+				subprocess.call(["./" + command, "assembleRelease"])
 		os.chdir("..")
 print("The following projects have updates:" + updated)
