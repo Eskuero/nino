9cd6202064c50d93dabe935dca9d1f150d6814a3
Óliver García Albertos <3skuero@gmail.com>
2018-12-01 11:55:13 +0000

    Move syncall to it's own repository
    The project has grown more than expected

--- twitter-cleanup/tweets.js
+++
@@ -1,6 +0,0 @@
-var tweets = document.getElementsByClassName("js-actionDelete");
-
-for (i = 0; i < tweets.length; i++) {
-	tweets[i].click();
-	document.getElementsByClassName("EdgeButton EdgeButton--danger delete-action")[0].click();
-}
