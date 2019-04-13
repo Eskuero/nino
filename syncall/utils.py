bb473f4d6c6c0409cc53d652ce1355e7468ee0f8
Óliver García Albertos <3skuero@gmail.com>
2019-04-13 19:50:14 +0100

    Simplify cmd arguments parsing
    Allow using capital "y" and "n" to enable and disable

--- syncall/utils.py
+++ syncall/utils.py
@@ -47,32 +47,23 @@ class utils():
 				sys.exit(1)
 			# Most arguments are boolean and follow the same logic
 			if name in rconfig:
-				if value == "y" and name not in ["deploy", "force"]:
+				# Deploy devices and forced projects are never boolean switches but lists to loop so just create the list
+				if name in ["deploy", "force"]:
+					rconfig[name] = value.split(",")
+				elif value in ["n", "N"]:
+					rconfig[name] = False
+				elif value in ["y", "Y"] or name == "build":
 					rconfig[name] = True
-				elif value == "n" and not name == "force":
-					if name == "deploy":
-						rconfig["deploy"] = []
-					else:
-						rconfig[name] = False
-				else:
-					# In the case of build/force we save a keystore/list respectively
+					# In the case of build we also save keystore information
 					if name == "build":
-						rconfig["build"] = True
 						value = value.split(",")
 						try:
-							keystores["overridestore"] = {
-								"path": value[0],
-								"aliases": {
-									value[1]: {}
-								}
-							}
+							keystores["overridestore"] = {"path": value[0],	"aliases": {value[1]: {}}}
 							rconfig["keystore"] = "overridestore"
 							rconfig["keyalias"] = value[1]
 						except IndexError:
 							print("No alias was given for keystore " + value[0] + " provided through command line")
 							sys.exit(1)
-					elif name in ["deploy", "force"]:
-						rconfig[name] = value.split(",")
-					else:
-						print("The argument " + arg[0] + " is expected boolean (y|n). Received: " + value)
-						sys.exit(1)
+				else:
+					print("The argument " + arg[0] + " is expected boolean (y|n). Received: " + value)
+					sys.exit(1)
