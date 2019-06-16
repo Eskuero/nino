7c2450150a05cbc46c61e0cdadad63157a56d2cd
Óliver García Albertos <3skuero@gmail.com>
2019-06-16 18:17:58 +0100

    Unavailability of fetching methods is not critical
    We can just drop the commands out of fetchmethods and report the user about it

--- nino/utils.py
+++ nino/utils.py
@@ -1,12 +1,16 @@
 import sys
 import shutil
-from .statics import dependencies
+from .statics import fetchmethods, dependencies
 
 def dpnds():
 	ready = True
 	for dep in dependencies:
 		if not shutil.which(dep):
-			ready = False
+			# Do not consider missing fetchmethods and critical failure and just disable it
+			if dep in ["git", "hg"]:
+				fetchmethods.pop("." + dep)
+			else:
+				ready = False
 			print("The required dependency '" + dep + "' is not in your PATH. Please refer to " + dependencies[dep])
 	if not ready:
 		sys.exit(1)
