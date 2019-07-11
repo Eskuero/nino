9b3158cae91b33c13500530ab514421f2e60dc29
Óliver García Albertos <3skuero@gmail.com>
2019-07-11 12:36:32 +0100

    Properly close logfile on each project

--- nino/__init__.py
+++ nino/__init__.py
@@ -26,18 +26,20 @@ def main():
 		app = project(name)
 		# Introduce the project
 		app.presentation()
-		# Sync the project
-		if app.sync:
-			app.fetch()
-		# Only attempt gradle projects with build enabled and are either forced or have new changes
-		if app.build and (app.changed or app.force):
-			app.package()
-		# We search for apks to sign and merge them to the current list
-		if app.built == 0 or app.signlist:
-			app.sign()
-		# We deploy if we built something
-		if app.deploylist:
-			app.install()
+		# Initialize logging to file for output of each operation
+		with open("log.txt", "w+") as app.logfile:
+			# Sync the project
+			if app.sync:
+				app.fetch()
+			# Only attempt gradle projects with build enabled and are either forced or have new changes
+			if app.build and (app.changed or app.force):
+				app.package()
+			# We search for apks to sign and merge them to the current list
+			if app.built == 0 or app.signlist:
+				app.sign()
+			# We deploy if we built something
+			if app.deploylist:
+				app.install()
 		# Store retriable config for project if not empty
 		if app.failed:
 			failed["projects"][name] = app.failed
